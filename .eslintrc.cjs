76cc3b85f4a6430ca456bca89dddca677d3229f3
Evli <ck18551458185@163.com>
2023-02-05 16:55:59 +0800

    初始化项目

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        singleQuote: true,
+        semi: false,
+        printWidth: 80,
+        trailingComma: "none",
+        endOfLine: "auto",
+      },
+    ],
+    "vue/multi-word-component-names": [
+      "warn",
+      {
+        ignores: ["index"],
+      },
+    ],
+    "vue/no-setup-props-destructure": ["off"],
+    "editor.codeActionsOnSave": {
+      "source.fixAll": true,
+    },
+  },
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+};
